87182c99f40734000207a23d6a52444e4ca3eaac
relu-desktop-2021 <relu1919@yahoo.com>
2022-01-23 13:25:26 +0200

    fb login test

--- src/components/facebook-login/index.js
+++ src/components/facebook-login/index.js
@@ -7,24 +7,12 @@ const FaceBookLogin = ({setUserDetails}) => {
     const responseFacebook = (response) => {
         createNewUser(response).then(() => setUserDetails(response))
     }
-
-    const isFacebookApp = () => {
-        const ua = navigator.userAgent || navigator.vendor || window.opera;
-        return (
-            ua.indexOf('FBAN') > -1 ||
-            ua.indexOf('FBAV') > -1 ||
-            ua.indexOf('Instagram') > -1
-        );
-    };
-
     return (
         <FacebookLogin
                 appId={process.env.REACT_APP_FACEBOOK_APP_ID}
                 fields="name,email,picture"
                 callback={responseFacebook}
-                disableMobileRedirect={!isFacebookApp()}
-                autoLoad={isFacebookApp()}
-                isMobile={false}
+                disableMobileRedirect={true}
                 cssClass='button'
             />
     )
