b85c81b3437e05c4fd89fee1decf7f93e7848ee8
relu-desktop-2021 <relu1919@yahoo.com>
2022-01-23 13:10:57 +0200

    messenger login test

--- src/components/facebook-login/index.js
+++ src/components/facebook-login/index.js
@@ -1,4 +1,4 @@
-import FacebookLogin from '@greatsumini/react-facebook-login';
+import FacebookLogin from "react-facebook-login";
 import {createNewUser} from "../../actions/users";
 
 const FaceBookLogin = ({setUserDetails}) => {
@@ -7,21 +7,24 @@ const FaceBookLogin = ({setUserDetails}) => {
     const responseFacebook = (response) => {
         createNewUser(response).then(() => setUserDetails(response))
     }
+
+    const isFacebookApp = () => {
+        const ua = navigator.userAgent || navigator.vendor || window.opera;
+        return (
+            ua.indexOf('FBAN') > -1 ||
+            ua.indexOf('FBAV') > -1 ||
+            ua.indexOf('Instagram') > -1
+        );
+    };
+
     return (
         <FacebookLogin
                 appId={process.env.REACT_APP_FACEBOOK_APP_ID}
                 fields="name,email,picture"
-                // onSuccess={responseFacebook}
-                style={{
-                    backgroundColor: '#4267b2',
-                    color: '#fff',
-                    fontSize: '16px',
-                    padding: '12px 24px',
-                    border: 'none',
-                    borderRadius: '4px',
-                    cursor: 'pointer'
-                }}
-                onProfileSuccess={responseFacebook}
+                callback={responseFacebook}
+                disableMobileRedirect={!isFacebookApp()}
+                autoLoad={isFacebookApp()}
+                cssClass='button'
             />
     )
 }
