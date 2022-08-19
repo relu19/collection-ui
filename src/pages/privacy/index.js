5cf3a269c0f3290804ca6d73146cb8a703ce8282
relu-desktop-2021 <relu1919@yahoo.com>
2022-08-19 13:14:16 +0300

    update privacy policy

--- src/pages/privacy/index.js
+++ src/pages/privacy/index.js
@@ -53,7 +53,7 @@ const Privacy = () => {
                 <h2>Children&apos;s Information</h2>
                 <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
                 <p>Collections does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
-
+                <p>You can delete all your stored data by sending an email to relu1919@yahoo.com</p>
        </div>
     )
 }
