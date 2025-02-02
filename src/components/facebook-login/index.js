8f420b17026070cb23a6d63a5d1461499417c43e
relu19 <relu.plesciuc@sfappworks.com>
2025-02-02 12:52:00 +0200

    packages updates

--- src/components/facebook-login/index.js
+++ src/components/facebook-login/index.js
@@ -1,20 +1,20 @@
-import FacebookLogin from "react-facebook-login";
-import {createNewUser} from "../../actions/users";
+// import FacebookLogin from "react-facebook-login";
+// import {createNewUser} from "../../actions/users";
 
 const FaceBookLogin = ({setUserDetails}) => {
 
+    //
+    // const responseFacebook = (response) => {
+    //     createNewUser(response).then(() => setUserDetails(response))
+    // }
 
-    const responseFacebook = (response) => {
-        createNewUser(response).then(() => setUserDetails(response))
-    }
-    return (
-        <FacebookLogin
-                appId={process.env.REACT_APP_FACEBOOK_APP_ID}
-                fields="name,email,picture"
-                callback={responseFacebook}
-                disableMobileRedirect={true}
-                cssClass='button'
-            />
-    )
+    return (<div></div>)
+    // <FacebookLogin
+    //         appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+    //         fields="name,email,picture"
+    //         callback={responseFacebook}
+    //         disableMobileRedirect={true}
+    //         cssClass='button'
+    //     />
 }
 export default FaceBookLogin
